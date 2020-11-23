a3e9bfdb6da2e45a02ffedab0134c8ec04dafc4b
Martti von Hertzen <martti.vonhertzen@gofore.com>
2020-11-23 16:16:41 +0200

    reverted copyright header changes

--- src/test/java/fi/evident/elasticsearch/voikko/analysis/VoikkoTokenFilterTests.java
+++ src/test/java/fi/evident/elasticsearch/voikko/analysis/VoikkoTokenFilterTests.java
@@ -1,20 +1,17 @@
 /*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
+ * Copyright 2013-2017 Evident Solutions Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package fi.evident.elasticsearch.voikko.analysis;
@@ -61,6 +58,7 @@ public class VoikkoTokenFilterTests extends ESTestCase {
             assumeTrue("System property 'voikko.path' is not defined, add '-Dvoikko.path=/path/to/voikko'", false);
             return;
         }
+
         Path morphology = dictDirectory.resolve("5/mor-morpho/mor.vfst");
 
         //noinspection Convert2Diamond
